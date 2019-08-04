23d8526469536849298fa3c01373e0671e9c5214
kkrntz <allan.d@electronicscience.com>
2019-08-04 20:23:32 +0800

    added conection details

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,41 @@
 var server = require('./server');
 
 exports.landingPage = function (req, res){
-    server.getServerAddress(function(ipAddress){
-        res.render('overview',{
-            ipAddress : ipAddress ? ipAddress : 'localhost',
-            pageName : 'overview'
-        });
-    });
+  server.getServerAddress(function(ipAddress){
+    server.getCurrentConnection(function(network, wifi){
+      res.render('overview', {
+        ipAddress : ipAddress ? ipAddress : 'localhost',
+        pageName : 'overview',
+        network : network,
+        wifi : wifi
+      });
+    })
+  });
 };
 
 exports.ftpPage = function(req, res){
-    server.getServerAddress(function(ipAddress){
-        res.render('ftp',{
-            ipAddress : ipAddress ? ipAddress : 'localhost',
-            pageName : 'ftp'
-        });
+  server.getServerAddress(function(ipAddress){
+    res.render('ftp',{
+      ipAddress : ipAddress ? ipAddress : 'localhost',
+      pageName : 'ftp'
     });
+  });
 };
 
 exports.connectionPage = function(req, res){
-    server.getServerAddress(function(ipAddress){
-        res.render('connection',{
-            ipAddress : ipAddress ? ipAddress : 'localhost',
-            pageName : 'connection'
-        });
+  server.getServerAddress(function(ipAddress){
+    res.render('connection',{
+      ipAddress : ipAddress ? ipAddress : 'localhost',
+      pageName : 'connection'
     });
+  });
 };
 
 exports.printerPage = function(req, res){
-    server.getServerAddress(function(ipAddress){
-        res.render('printer',{
-            ipAddress : ipAddress ? ipAddress : 'localhost',
-            pageName : 'printer'
-        });
+  server.getServerAddress(function(ipAddress){
+    res.render('printer',{
+      ipAddress : ipAddress ? ipAddress : 'localhost',
+      pageName : 'printer'
     });
+  });
 };
--- routes/server.js
+++ routes/server.js
@@ -1,5 +1,36 @@
-var ip = require('ip');
+var wifi = require('node-wifi');
+var network = require('network');
+
+wifi.init();
 
 exports.getServerAddress = function(callback){
-    callback(ip.address());
+  network.get_private_ip(function(err, ip){
+    if(err){
+      callback(false);
+    }else{
+      callback(ip);
+    }
+  })
 };
+
+exports.getCurrentConnection = function(callback){
+  network.get_active_interface(function(err, interface){
+    if(err){
+      callback(err);
+    }else{
+      wifi.getCurrentConnections(function(err2, networks) {
+        console.log(interface);
+        console.log(networks);
+        if (err2) {
+          callback(false);
+        } else {
+          if(networks.length > 0){
+            callback(interface, networks[0]);
+          }else{
+            callback(interface, null);
+          }
+        }
+      });
+    }
+  });
+}
